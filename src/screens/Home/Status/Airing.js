1e9e86383b7aaeed21527201bf60e85439248056
Mohamad Syazwan Shafei <syazwan567@gmail.com>
2024-04-27 14:32:53 +0800

    Add react navigation screens

---
+++ src/screens/Home/Status/Airing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Airing = () => {
+  return (
+    <View>
+      <Text>This is Anime list with airing status screen</Text>
+    </View>
+  );
+};
+
+export default Airing;
---
+++ src/screens/Home/Status/Upcoming.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Upcoming = () => {
+  return (
+    <View>
+      <Text>This is Anime list with upcoming status screen</Text>
+    </View>
+  );
+};
+
+export default Upcoming;
